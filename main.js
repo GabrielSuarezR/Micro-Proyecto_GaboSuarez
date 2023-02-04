800e83f83f2bab18395f9302f274559e78cc7b87
GabrielSuarez <suarezgabrielr@gmail.com>
2023-02-04 15:59:49 -0400

    slider funcional

---
+++ main.js
@@ -0,0 +1,26 @@
+// js del Hero
+function myFunc(){
+    alert("You Clicked me");
+ }
+
+const swiper = new Swiper('.swiper', {
+    autoplay:{
+        delay:5000,
+        disableOnInteraction:false,
+    },
+    loop: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
+  
+
+  
